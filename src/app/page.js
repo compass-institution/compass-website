7733baa1327ef9aee9c4745ac7fd1c86185d2519
Ava-Rubin <avarubin@gmail.com>
2024-01-30 23:07:15 -0800

    added most of global styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      COMPASS
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+     
+    {/*TODO: MOVE ALL BELOW ONTO HOME PAGE -- HERE FOR EXAMPLE */}
+
+      <div className="relative">
+          <div className="gradient-orb top-orb"></div>
+          <div className="gradient-orb mid-orb"></div>
+      </div>
+
+      <div className="page-header-div">
+        <h1>COMPASS Institution</h1>
+        <h3>MATHEMATICAL MODELING FOR SOCIAL GOOD</h3>
+        <p className="body-text sub-description">
+          We're a student run organization partnering with local and international organizations to amplify their impact through mathematics, computer science and data science.
+        </p>
+      </div>
     </>
+    
   );
 }
